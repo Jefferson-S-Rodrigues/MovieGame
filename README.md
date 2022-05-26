59c692bd77a27dc9c19f4172ed89fd5a4bf34e06
Jefferson-S-Rodrigues <jefferson2jsr@gmail.com>
2022-05-26 09:23:36 -0300

    readme

---
+++ README.md
@@ -0,0 +1,7 @@
+# Movie Game
+
+É um jogo em que você deve escolher o filme mais votado.
+
+### Swagger Local
+
+<http://localhost:8080/swagger-ui.html>
--- src/test/java/tk/jeffersondev/moviegame/service/MovieServiceTest.java
+++ src/test/java/tk/jeffersondev/moviegame/service/MovieServiceTest.java
@@ -5,13 +5,10 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import tk.jeffersondev.moviegame.entity.Game;
 import tk.jeffersondev.moviegame.utils.MovieUtils;
-import tk.jeffersondev.moviegame.utils.ShotUtils;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 
 @ExtendWith(SpringExtension.class)
