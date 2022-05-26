4cac28a9b900772475ece0e76cd8447db65c24a5
Jefferson-S-Rodrigues <jefferson2jsr@gmail.com>
2022-05-26 08:42:47 -0300

    Movie service unit test

---
+++ src/test/java/tk/jeffersondev/moviegame/service/MovieServiceTest.java
@@ -0,0 +1,35 @@
+package tk.jeffersondev.moviegame.service;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import tk.jeffersondev.moviegame.entity.Game;
+import tk.jeffersondev.moviegame.utils.MovieUtils;
+import tk.jeffersondev.moviegame.utils.ShotUtils;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+class MovieServiceTest {
+
+    @MockBean
+    private MovieService movieService;
+
+    @Test
+    void randomMovieTest() {
+
+        given(movieService.randomMovie()).willReturn(MovieUtils.fakeMovie1());
+
+        var movie = movieService.randomMovie();
+
+        assertEquals("movie", movie.getType());
+        assertEquals(10, movie.getImdbID().length());
+        assertFalse(movie.getTitle().isEmpty());
+    }
+}
